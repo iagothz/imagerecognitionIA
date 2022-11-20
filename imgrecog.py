e80d47d0af5513e0551f0549d15b7d6b8d27a3fd
Iago <iago.formes.r@gmail.com>
2022-11-20 10:21:35 -0300

    Update imgrecog.py

--- imgrecog.py
+++ imgrecog.py
@@ -7,6 +7,6 @@ prediction.setModelPath( execution_path + "\resnet50_imagenet_tf.2.0.h5")
 prediction.loadModel()
 
 
-predictions, percentage_probabilities = prediction.classifyImage("C:\Users\iagoz\Desktop\Github\Image Recognition\carro vermelho.jpg", result_count=5)
+predictions, percentage_probabilities = prediction.classifyImage("img\carro vermelho.jpg", result_count=5)
 for index in range(len(predictions)):
   print(predictions[index] , " : " , percentage_probabilities[index])
